a03f9374c23ef1062b091527ce70c672815285b1
yashkvr <yashincrypto5@gmail.com>
2024-06-16 18:02:10 +0530

    added shadcn ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
 
 const Hero = () => {
     return (
-        <div className='w-dvw h-dvh flex flex-col items-center justify-center px-[3%] space-y-5'>
+        <div className='w-dvw h-dvh flex flex-col items-center justify-center px-[3%] space-y-5 bg-[#FFF5E1]'>
             <p className='text-4xl text-bold'>THE FUTURE OF BATTERY TECHNOLOGY</p>
             <p className='text-2xl text-semibold'>Discover The Right Battery and Electrification Solutions With RIVAA.</p>
-            <button className='py-1 px-3 bg-black text-white hover:bg-gray-700 rounded-md'>Call To Action</button>
+            {/* <button className='py-1 px-3 bg-black text-white hover:bg-gray-700 rounded-md'>Call To Action</button> */}
+            <Button variant='default' size='lg'>Call To Action</Button>
         </div>
     )
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Services = () => {
     return (
-        <div className='w-dvw h-dvh flex flex-col items-center pt-[5%]'>
+        <div className='w-dvw h-dvh flex flex-col items-center pt-[5%] bg-[#FFF5E1]'>
             <p className='text-4xl font-bold'>Our Services</p>
             <div className="flex w-[90%] my-10 h-[60%]  justify-between">
-                <div className='border border-black rounded-lg'>
+                <div className='border border-black rounded-lg px-2'>
                     Services card for this section
                 </div>
-                <div className='border border-black rounded-lg'>
+                <div className='border border-black rounded-lg px-2'>
                     Services card for this section
                 </div>
-                <div className='border border-black rounded-lg'>
+                <div className='border border-black rounded-lg px-2'>
                     Services card for this section
                 </div>
-                <div className='border border-black rounded-lg'>
+                <div className='border border-black rounded-lg px-2'>
                     Services card for this section
                 </div>
             </div>
